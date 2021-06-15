10a12efb902758ac28e09fe48e64890d76b74a94
007HarshChaudhary <theheroisbackagain@gmail.com>
2021-06-15 14:43:43 +0530

    UI and icon changes

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -8,11 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class ContactComponent implements OnInit {
 
   socialLinks = [
-    {ref: "https://github.com/007HarshChaudhary", src:"assets/github.png", alt:"github"},
-    {ref: "https://www.youtube.com/channel/UCd1rWoJN3v9rONdgVNQmwnQ", src:"assets/yt_icon.png", alt:"youtube"},
-    {ref: "https://www.linkedin.com/in/harshachaudhary007/", src:"assets/linkedin.png", alt:"linkedIn"},
-    {ref: "https://www.instagram.com/its._.me_harsh/", src:"assets/insta.png", alt:"instagram"},
-    {ref: "https://www.facebook.com/001harsh", src:"assets/fb_icon.png", alt:"facebook"},
+    {ref: "https://github.com/007HarshChaudhary", src:"assets/github.png", alt:"Github"},
+    {ref: "https://www.youtube.com/channel/UCd1rWoJN3v9rONdgVNQmwnQ", src:"assets/yt_icon.png", alt:"YouTube"},
+    {ref: "https://www.linkedin.com/in/harshachaudhary007/", src:"assets/linkedin.png", alt:"LinkedIn"},
+    {ref: "https://www.instagram.com/its._.me_harsh/", src:"assets/insta.png", alt:"Instagram"},
+    {ref: "https://www.facebook.com/001harsh", src:"assets/fb_icon.png", alt:"Facebook"},
   ];
   constructor() { }
 
